d851093caaaa774e3bfcfd68f23a02c8acce21f9
Albertcleetus <38793369+Albertcleetus@users.noreply.github.com>
2024-09-14 01:05:37 +0530

    branch combinations

--- Assets/Scripts/branchScreen.cs
+++ Assets/Scripts/branchScreen.cs
@@ -8,7 +8,7 @@ public class branchScreen : MonoBehaviour
 
     private SpriteRenderer _renderer;
 
-    public Sprite[] branchlist;
+    public Sprite[] branchlist; // Total of 27 sprites in this array
     private void Awake()
     {
         // Get the background setting from PlayerPrefs
@@ -17,75 +17,75 @@ public class branchScreen : MonoBehaviour
         // Get the SpriteRenderer component
         _renderer = GetComponent<SpriteRenderer>();
 
-        // Check different background sets and assign the corresponding sprite
+        // Check different background sets and assign a random sprite from the corresponding range
         switch (bgsetName)
         {
             case "defaultBackgrounds": // bgset1
-                _renderer.sprite = branchlist[0];
+                _renderer.sprite = branchlist[Random.Range(0, 3)]; // Random between branchlist[0] - branchlist[2]
                 break;
 
-            case "bgset2":
-                _renderer.sprite = branchlist[1];
+            case "bgset2": // bgset2
+                _renderer.sprite = branchlist[Random.Range(3, 6)]; // Random between branchlist[0] - branchlist[2]
                 break;
 
-            case "bgset3":
-                _renderer.sprite = branchlist[1];
+            case "bgset3": // bgset3
+                _renderer.sprite = branchlist[Random.Range(3, 6)]; // Random between branchlist[0] - branchlist[2]
                 break;
 
-            case "bgset4":
-                _renderer.sprite = branchlist[2];
+            case "bgset4": // bgset4
+                _renderer.sprite = branchlist[Random.Range(6, 9)]; // Random between branchlist[3] - branchlist[4]
                 break;
 
-            case "bgset5":
-                _renderer.sprite = branchlist[2];
+            case "bgset5": // bgset5
+                _renderer.sprite = branchlist[Random.Range(6, 9)]; // Random between branchlist[3] - branchlist[4]
                 break;
 
-            case "bgset6":
-                _renderer.sprite = branchlist[3];
+            case "bgset6": // bgset6
+                _renderer.sprite = branchlist[Random.Range(9, 12)]; // Random between branchlist[5] - branchlist[6]
                 break;
 
-            case "bgset7":
-                _renderer.sprite = branchlist[3];
+            case "bgset7": // bgset7
+                _renderer.sprite = branchlist[Random.Range(9, 12)]; // Random between branchlist[5] - branchlist[6]
                 break;
 
-            case "bgset8":
-                _renderer.sprite = branchlist[4];
+            case "bgset8": // bgset8
+                _renderer.sprite = branchlist[Random.Range(12, 15)]; // Random between branchlist[7] - branchlist[8]
                 break;
 
-            case "bgset9":
-                _renderer.sprite = branchlist[4];
+            case "bgset9": // bgset9
+                _renderer.sprite = branchlist[Random.Range(12, 15)]; // Random between branchlist[7] - branchlist[8]
                 break;
 
-            case "bgset10":
-                _renderer.sprite = branchlist[5];
+            case "bgset10": // bgset10
+                _renderer.sprite = branchlist[Random.Range(15, 18)]; // Random between branchlist[9] - branchlist[10]
                 break;
 
-            case "bgset11":
-                _renderer.sprite = branchlist[5];
+            case "bgset11": // bgset11
+                _renderer.sprite = branchlist[Random.Range(15, 18)]; // Random between branchlist[9] - branchlist[10]
                 break;
 
-            case "bgset12":
-                _renderer.sprite = branchlist[6];
+            case "bgset12": // bgset12
+                _renderer.sprite = branchlist[Random.Range(18, 21)]; // Random between branchlist[11] - branchlist[12]
                 break;
 
-            case "bgset13":
-                _renderer.sprite = branchlist[6];
+            case "bgset13": // bgset13
+                _renderer.sprite = branchlist[Random.Range(18, 21)]; // Random between branchlist[11] - branchlist[12]
                 break;
 
-            case "bgset14":
-                _renderer.sprite = branchlist[7];
+            case "bgset14": // bgset14
+                _renderer.sprite = branchlist[Random.Range(21, 24)]; // Random between branchlist[13] - branchlist[14]
                 break;
 
-            case "bgset15":
-                _renderer.sprite = branchlist[7];
+            case "bgset15": // bgset15
+                _renderer.sprite = branchlist[Random.Range(21, 24)]; // Random between branchlist[13] - branchlist[14]
                 break;
 
-            case "bgset16":
-                _renderer.sprite = branchlist[8];
+            case "bgset16": // bgset16
+                _renderer.sprite = branchlist[Random.Range(24, 27)]; // Random between branchlist[15] - branchlist[16]
                 break;
 
-            case "bgset17":
-                _renderer.sprite = branchlist[8];
+            case "bgset17": // bgset17
+                _renderer.sprite = branchlist[Random.Range(24, 27)]; // Random between branchlist[15] - branchlist[16]
                 break;
 
             default:
@@ -94,10 +94,8 @@ public class branchScreen : MonoBehaviour
         }
     }
 
-
-
     void Update()
     {
-        
+        // Optional updates if necessary
     }
 }
